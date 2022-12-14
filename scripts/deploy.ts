ae975e1e31f38cdf892b6b9599d1bd9849b006c4
robin <robinnagpal.tiet@gmail.com>
2022-12-14 11:32:29 -0500

    some clean ups

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,41 +1,10 @@
+import fs from "fs";
+import path from "path";
 import { artifacts, ethers } from "hardhat";
 import { Token } from "./../typechain-types/contracts/Token";
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = ethers.utils.parseEther("0.001");
-
-  const Token = await ethers.getContractFactory("Token");
-  const token = await Token.deploy();
-  await token.deployed();
-
-  console.log("Token address:", token.address);
-
-  // We also save the contract's artifacts and address in the frontend directory
-  saveFrontendFiles(token);
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${token.address}`
-  );
-}
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
-
-// This is a script for deploying your contracts. You can adapt it to deploy
-// yours, or create new ones.
-
-const path = require("path");
 
 function saveFrontendFiles(token: Token) {
-  const fs = require("fs");
   const contractsDir = path.join(
     __dirname,
     "..",
@@ -71,6 +40,26 @@ function saveFrontendFiles(token: Token) {
       "typechain-types"
     ),
     { recursive: true }
+  );
+}
+
+
+async function main() {
+  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+
+  const Token = await ethers.getContractFactory("Token");
+  const token = await Token.deploy();
+  await token.deployed();
+
+  console.log("Token address:", token.address);
+
+  // We also save the contract's artifacts and address in the frontend directory
+  saveFrontendFiles(token);
+
+  console.log(
+    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${token.address}`
   );
 }
 
